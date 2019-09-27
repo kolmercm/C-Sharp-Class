84fbfc892d3d4c45738bf8b52de549de4c5739fe
Conor Kolmer <47374116+kolmercm@users.noreply.github.com>
2019-09-27 00:15:21 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GoCards
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                Console.WriteLine();
+            
+                if (i % 4 == 0 & i % 7 == 0)
+                {
+                    Console.Write("Go Cards");
+                
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Cards");
+                }
+                else if (i % 4 == 0)
+                {
+                    Console.Write("Go");
+                }
+                else
+                Console.WriteLine(i);
+            }
+            
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
